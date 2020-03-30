7e7ebb1e9311c96b6caa7821d7f8f6881c5171b0
d-mindlin <mindlin@ucsb.com>
2020-03-29 20:50:46 -0700

    fixed css error and error preventing password from being printed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,107 +1,67 @@
 // Assignment code here
+var generatePassword = function() 
+{
+  var randomString = "";
+  var specialChar = " !@#$%^&*()_+'?~+[]{} ";
+  var number = " 0123456789 ";
+  var lowerAlpha = " abcdefghijklmnopqrstuvwxyz ";
+  var upperAlpha = " ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+  // Ask user to pick a lenght for the password between 8 to 128
+  var passwordLength = parseInt(window.prompt("Please enter the desired length of the password between 8 to 128."));
+  var passwordString = "";
 
-// Random Values START
-// prettier-ignore
-var lowercase= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-// prettier-ignore
-var uppercase= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var spec = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-var special = spec.split("");
-var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-// Random Values END
+  //check if the length of the password is long enough
+  if (passwordLength >= 8 && passwordLength < 128) {
+    // ask th user if they want to have special characters
+    var useSpecialChar = (window.prompt("Do you wish to include special characters in your password? Type yes to confirm.")).toLowerCase();
+    //if yes
+    if(useSpecialChar === "yes") {
+      randomString = randomString + specialChar;
+      console.log(randomString);
+    }
 
-console.log(lowercase);
-console.log(uppercase);
-console.log(special);
-console.log(nums);
+    //ask the user if they want to have numbers 
+    var useNumber = (window.prompt("do you wish to include Numbers in your password? Type yes to confirm.")).toLowerCase();
+    //if yes
+    if (useNumber === "yes") {
+      randomString = randomString + number;
+      console.log(randomString);
+    }
 
-// Password Condition START
-var passwordConditions = function() {
-  var passLength = "";
-  while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
-    passLength = prompt("Choose a password with and length between (8 - 128)");
-  }
-  // ^ Will force you to pick a number between 8 - 128 and won't allow non numbers
-  var lowerCheck = "";
-  while (lowerCheck === "") {
-    lowerCheck = confirm("Would you like to include LOWERcase letters?");
-  }
+    //ask user if they want to have lower case alphabets
+    var useLowerBets = (window.prompt("Do you wish to include lower case alphabets in your password? Type yes to confirm.")).toLowerCase();
+    //if yes
+    if (useLowerBets === "yes") {
+      randomString = randomString + lowerAlpha;
+      console.log (randomString);
+    }
 
-  var upperCheck = "";
-  while (upperCheck === "") {
-    upperCheck = confirm("Would you like to include UPPERcase letters?");
+    //ask user if they want to have upper case alphabets
+    var useUpperBets = (window.prompt("Do you wish to include Upper case alphabets in your password? Type yes to confirm.")).toLowerCase();
+    //if yes
+    if (useUpperBets === "yes") {
+      randomString = randomString + upperAlpha;
+      console.log (randomString);
+    } 
+  } 
+  else {
+    window.alert ( " Your Password does not meet the basic requirements. Please Try Again");
   }
+  
+  const typesCount = useSpecialChar + useLowerBets +useUpperBets + useNumber;
 
-  var numsCheck = "";
-  while (numsCheck === "") {
-    numsCheck = confirm("Would you like to include NUMBERS?");
+  if (typesCount === 0){
+   return '';
   }
-
-  var symbolCheck = "";
-  while (symbolCheck === "") {
-    symbolCheck = confirm("Would you like to include SYMBOLS?");
-  }
-  // Password Conidtion END
-
-  // console log to confirm arrays are working in inspect element
-  console.log(lowerCheck);
-  console.log(upperCheck);
-  console.log(numsCheck);
-  console.log(symbolCheck);
-
-  // if you pick no password condition then the program will restart and give an error message
-  if (
-    lowerCheck === false &&
-    upperCheck === false &&
-    numsCheck === false &&
-    symbolCheck === false
-  ) {
-    alert("Please include at least one password option.");
-    passwordConditions();
-  } else {
-    alert("Valid password choosen.");
-  }
-
-  // Will check if password conditions where chosen by the user
-  // and include them in the creation of a random password
-  var charsAllowed = [];
-  var thestring = "";
-  // only including variables that the user chooses to include into the password
-  if (lowerCheck === true) {
-    thestring += lowercase[Math.floor(Math.random() * lowercase.length)];
-    charsAllowed = [...charsAllowed, ...lowercase];
-  }
-  if (upperCheck === true) {
-    thestring += uppercase[Math.floor(Math.random() * uppercase.length)];
-    charsAllowed = [...charsAllowed, ...uppercase];
-  }
-  if (numsCheck === true) {
-    thestring += Math.floor(Math.random() * 9);
-    charsAllowed = [...charsAllowed, ...nums];
-  }
-  if (symbolCheck === true) {
-    thestring += special[Math.floor(Math.random() * special.length)];
-    charsAllowed = [...charsAllowed, ...special];
-  }
-
-  var newstring = "";
-  for (var i = 0; i < passLength - thestring.length; i++) {
-    newstring += charsAllowed[Math.floor(Math.random() * charsAllowed.length)];
-  } // += means newstring = newstring + charsAllowed
-  console.log(newstring);
-
-  // makes sure that included password variables aren't left out
-  for (var i = 0; i < thestring.length; i++) {
-    position = Math.floor(Math.random() * newstring.length);
-    newstring = [
-      newstring.slice(0, position),
-      thestring[i],
-      newstring.slice(position)
-    ].join("");
-  }
-  console.log(newstring);
-  alert(newstring);
-};
+     //generate random password based on the characters the user picked
+    for (var i = 0; i < passwordLength; i++){
+      passwordString += randomString.charAt(Math.floor(Math.random() * randomString.length));
+    }
+      //return passwordString;
+      const finalPassword = passwordString;//.slice(0, passwordLength);
+      console.log(finalPassword);
+      return finalPassword;   
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -112,6 +72,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
